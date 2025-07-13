d2edec4c85361dbf12d54f784cb732b2ad191832
mfrisch1 <mitchellfrisch1@gmail.com>
2025-07-13 16:03:36 +1000

    Feature: cmp, autopair. Update: mason

--- lua/mitchell/plugins/mason.lua
+++ lua/mitchell/plugins/mason.lua
@@ -1,40 +1,44 @@
 return {
 	{
 		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			{ "mason-org/mason.nvim",
+				opts = {},
+				build = ":MasonUpdate",
+				config = true
+			},
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+		},
 		opts = {
 			ensure_installed = {
 				"clangd",
 				"lua_ls",
 				"gopls",
 				"bashls",
-				"jsonls",
-				"yamlls"
+				"biome",
+				"yamlls",
+				"ts_ls",
+				"tailwindcss-language-server",
+				"css-variables-language-server",
 			},
+			automatic_installation = true,
+			automatic_enable = true,
 		},
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-		},
-		config = function()
+		config = function(_, opts)
 			require("mason").setup()
-			require("mason-lspconfig").setup({
-				automatic_enable = true,
-			})
-			local lsp = vim.lsp
-			lsp.enable("gopls")
-			lsp.enable("clangd")
-
-			lsp.config("lua_ls", {
+			require("mason-lspconfig").setup({opts})
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			require("lspconfig").lua_ls.setup({
+				capabilities = capabilities,
 				settings = {
 					Lua = {
 						diagnostics = {
 							globals = { "vim" },
-						},
-					},
-				},
+						}
+					}
+				}
 			})
-			lsp.enable("lua_ls")
-
-		end
+		end,
 	}
 }
